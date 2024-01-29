c05642522918d139e4905e67fb3f676375200463
Valeri Stoyanov <valeri.n.stoqnov@gmail.com>
2024-01-29 11:31:05 +0200

    Added single-portfolio.php
    Design single-portfolio.php

--- wp-content/themes/portfolio/single-portfolio.php
+++ wp-content/themes/portfolio/single-portfolio.php
@@ -45,7 +45,6 @@ get_header();
                 );
                 $the_query = new WP_Query( $args );
                 if ( $the_query->have_posts() ) :
-
                     while ( $the_query->have_posts() ) :
                         $the_query->the_post();
                         $bookDescription = get_field('book_description'); ?>
